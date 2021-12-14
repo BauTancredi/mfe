0a7b9d4b1894585ae4d19c5e23e0de70ffbbad48
BauTancredi <bautistatancredi@hotmail.com>
2021-12-14 11:09:04 -0300

    Fixing webpack configs

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,6 +3,8 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
+const domain = process.env.PRODUCTION_DOMAIN;
+
 const prodConfig = {
   mode: "production",
   output: {
@@ -11,8 +13,8 @@ const prodConfig = {
   plugins: [
     new ModuleFederationPlugin({
       name: "container",
-      exposes: {
-        "./MarketingApp": "./src/bootstrap",
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
